76e221ef69c0d028813b7abcb031da08add58b98
onnesa <onnes@qq.com>
2017-09-28 17:59:46 +0800

    c6

--- testb/src/main/java/com/onnes/testb/api/RetrofitService.java
+++ testb/src/main/java/com/onnes/testb/api/RetrofitService.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 /**
- * Created by Administrator on 2017/9/25.
+ * Created by Administrator on 2017/9/25.a
  */
 
 public interface RetrofitService {
